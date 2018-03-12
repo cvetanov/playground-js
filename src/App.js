61cba1701e2293d44f027713aceae8aee40ba9ff
Goce Cvetanov <Goce.Cvetanov@netcetera.com>
2018-03-12 17:09:36 +0100

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import Home from "./Home";
 
 class App extends Component {
   render() {
@@ -13,6 +15,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Router>
+          <Route exact path="/" component={Home} />
+        </Router>
+
       </div>
     );
   }
